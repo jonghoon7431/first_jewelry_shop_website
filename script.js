e3bf20ec0d7126c7fc1bb2e23cd6961f282e6e9e
jonghoon <jonghoon7431@gmail.com>
2024-02-28 18:20:57 +0900

    2.28

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const slidePrevBtn = document.querySelector(".prev_button");
 const slideNextBtn = document.querySelector(".next_button");
 
 let slideImg = ["images/black300px.png", "images/white300px.png"];
+let currentIndex = 0;
 
 slideNextBtn.addEventListener("click", () => {
   let img = document.getElementById("slide_img");
-  img.src = `./${slideImg}`;
+  currentIndex = (currentIndex + 1) % slideImg.length;
+  img.src = `./${slideImg[currentIndex]}`;
 });
+
+slidePrevBtn.addEventListener("click", () => {
+  let img = document.getElementById("slide_img");
+  currentIndex = (currentIndex - 1 + slideImg.length) % slideImg.length;
+  img.src = `./${slideImg[currentIndex]}`;
+});
+
+//모든 버튼이 동일 작동 하게 만들 방법
+//좌우 이동 드래그 슬라이더 생성
